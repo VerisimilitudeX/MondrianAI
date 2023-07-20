76f08c6a7b4272b312e5d69ded72db2e510b1275
Piyush Acharya <acharyapiyush1@outlook.com>
2023-07-20 10:14:49 +0530

    Basic working solution

--- Mondrian.java
+++ Mondrian.java
@@ -4,99 +4,54 @@ import java.util.Random;
 public class Mondrian {
     private int canvasHeight;
     private int canvasWidth;
-    private boolean dimensionsSet;
 
-    public Mondrian() {
-        dimensionsSet = false;
-    }
+    public Mondrian() {}
 
     public void paintBasicMondrian(Color[][] pixels) {
-        int height = pixels.length;
-        int width = pixels[0].length;
+        this.canvasHeight = pixels.length;
+        this.canvasWidth = pixels[0].length;
+        
+        draw(pixels, 0, 0, canvasWidth, canvasHeight);
+    }
 
-        if (!dimensionsSet) {
-            canvasHeight = height;
-            canvasWidth = width;
-            dimensionsSet = true;
-        }
-
+    private void draw(Color[][] pixels, int x0, int y0, int width, int height) {
         if (height < 10 || width < 10) {
             return;
         }
 
         if (height >= 0.25 * canvasHeight && width >= 0.25 * canvasWidth) {
-            int heightRange = height - 10;
-            int heightSplit = (int) (Math.random() * heightRange) + 10;
+            int heightSplit = (int) (Math.random() * (height - 10)) + 10;
+            int widthSplit = (int) (Math.random() * (width - 10)) + 10;
 
-            int widthRange = width - 10;
-            int widthSplit = (int) (Math.random() * widthRange) + 10;
-
-            Color[][] region1 = new Color[heightSplit][widthSplit];
-            paintBasicMondrian(region1);
-
-            Color[][] region2 = new Color[heightSplit][width - widthSplit];
-            paintBasicMondrian(region2);
-
-            Color[][] region3 = new Color[height - heightSplit][widthSplit];
-            paintBasicMondrian(region3);
-
-            Color[][] region4 = new Color[height - heightSplit][width - widthSplit];
-            paintBasicMondrian(region4);
-
-            for (int i = 0; i < height; i++) {
-                for (int j = 0; j < width; j++) {
-                    if (i < heightSplit && j < widthSplit) {
-                        pixels[i][j] = region1[i][j];
-                    } else if (i < heightSplit && j >= widthSplit) {
-                        pixels[i][j] = region2[i][j - widthSplit];
-                    } else if (i >= heightSplit && j < widthSplit) {
-                        pixels[i][j] = region3[i - heightSplit][j];
-                    } else {
-                        pixels[i][j] = region4[i - heightSplit][j - widthSplit];
-                    }
-                }
-            }
+            draw(pixels, x0, y0, widthSplit, heightSplit);
+            draw(pixels, x0+widthSplit, y0, width-widthSplit, heightSplit);
+            draw(pixels, x0, y0+heightSplit, widthSplit, height-heightSplit);
+            draw(pixels, x0+widthSplit, y0+heightSplit, width-widthSplit, height-heightSplit);
         } else if (height >= 0.25 * canvasHeight) {
-            int heightRange = height - 10;
-            int heightSplit = (int) (Math.random() * heightRange) + 10;
+            int heightSplit = (int) (Math.random() * (height - 10)) + 10;
 
-            for (int i = 0; i < height; i++) {
-                for (int j = 0; j < width; j++) {
-                    if (i == heightSplit) {
-                        pixels[i][j] = Color.BLACK;
-                    } else if (i > heightSplit) {
-                        pixels[i][j] = Color.WHITE;
-                    } else {
-                        pixels[i][j] = Color.RED;
-                    }
-                }
-            }
+            draw(pixels, x0, y0, width, heightSplit);
+            draw(pixels, x0, y0+heightSplit, width, height-heightSplit);
         } else if (width >= 0.25 * canvasWidth) {
-            int widthRange = width - 10;
-            int widthSplit = (int) (Math.random() * widthRange) + 10;
+            int widthSplit = (int) (Math.random() * (width - 10)) + 10;
 
-            for (int i = 0; i < height; i++) {
-                for (int j = 0; j < width; j++) {
-                    if (j == widthSplit) {
-                        pixels[i][j] = Color.BLACK;
-                    } else if (j > widthSplit) {
-                        pixels[i][j] = Color.WHITE;
-                    } else {
-                        pixels[i][j] = Color.YELLOW;
-                    }
-                }
-            }
+            draw(pixels, x0, y0, widthSplit, height);
+            draw(pixels, x0+widthSplit, y0, width-widthSplit, height);
         } else {
             Color[] colors = {Color.RED, Color.YELLOW, Color.CYAN, Color.WHITE};
             Random random = new Random();
             Color randomColor = colors[random.nextInt(colors.length)];
 
-            for (int i = 0; i < height; i++) {
-                for (int j = 0; j < width; j++) {
-                    if (i == 0 || j == 0 || i == height - 1 || j == width - 1) {
+            for (int i = y0+1; i < y0+height-1; i++) {
+                for (int j = x0+1; j < x0+width-1; j++) {
+                    pixels[i][j] = randomColor;
+                }
+            }
+            // Draw border
+            for (int i = y0; i < y0+height; i++) {
+                for (int j = x0; j < x0+width; j++) {
+                    if (i==y0 || j==x0 || i==y0+height-1 || j==x0+width-1) {
                         pixels[i][j] = Color.BLACK;
-                    } else {
-                        pixels[i][j] = randomColor;
                     }
                 }
             }
@@ -104,6 +59,6 @@ public class Mondrian {
     }
 
     public void paintComplexMondrian(Color[][] pixels) {
-        // TODO: Implement the complex Mondrian art generator
+        
     }
 }
